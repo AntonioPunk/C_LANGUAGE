1c38a8d7e44b107cb2cafe91f99dab0b2596b69d
Antonio Arias Ureta <kiosco.rock@hotmail.com>
2025-03-23 11:47:01 +0100

    Update

--- prueba.c
+++ prueba.c
@@ -4,7 +4,7 @@ int main(){
 
     float number;
     printf("Introduzca un número: ");
-    scanf("%f, %*c", &number);
+    scanf("%f%*c", &number);
     printf("\nHa introducido el número: %.2f", number);
 
     return 0;
