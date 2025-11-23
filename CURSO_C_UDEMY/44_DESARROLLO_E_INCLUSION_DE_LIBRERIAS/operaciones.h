e583ed83c2b44ba09b036b90b8210bad04943cb7
AntonioPunk <kiosco.rock@hotmail.com>
2025-11-23 20:57:28 +0100

    Delete All

--- CURSO_C_UDEMY/44_DESARROLLO_E_INCLUSION_DE_LIBRERIAS/operaciones.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPERACIONES_H_INCLUDED
-#define OPERACIONES_H_INCLUDED
-
-int suma(int num1, int num2);
-int resta(int num1, int num2);
-int multiplicacion(int num1, int num2);
-float division(int num1, int num2);
-
-#endif
