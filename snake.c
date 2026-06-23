23e2b4b7493b60a9ba53c6a5c2d1fe744bcf7810
AntonioPunk <kiosco.rock@hotmail.com>
2026-06-23 20:51:15 +0200

    Refactor snake game logic and improve fruit placement functionality

--- snake.c
+++ snake.c
@@ -1,3 +1,5 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
@@ -5,12 +7,45 @@
 
 #define WIDTH 20
 #define HEIGHT 20
+#define MAX_TAIL (WIDTH * HEIGHT - 1)
+#define INITIAL_DELAY_US 100000
+#define MIN_DELAY_US 30000
+#define DELAY_STEP_US 2500
 
 int x, y, fruitX, fruitY, score;
-int tailX[100], tailY[100];
+int tailX[MAX_TAIL], tailY[MAX_TAIL];
 int nTail;
-enum eDirecton { STOP = 0, LEFT, RIGHT, UP, DOWN };
-enum eDirecton dir;
+int gameOver;
+int frameDelayUs;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+enum eDirection dir;
+
+void SleepMicroseconds(int microseconds);
+
+int IsCellOccupied(int cellX, int cellY) {
+    if (x == cellX && y == cellY) {
+        return 1;
+    }
+
+    for (int i = 0; i < nTail; i++) {
+        if (tailX[i] == cellX && tailY[i] == cellY) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+void PlaceFruit() {
+    if (nTail + 1 >= WIDTH * HEIGHT) {
+        return;
+    }
+
+    do {
+        fruitX = rand() % WIDTH;
+        fruitY = rand() % HEIGHT;
+    } while (IsCellOccupied(fruitX, fruitY));
+}
 
 void Setup() {
     initscr();
@@ -21,14 +56,15 @@ void Setup() {
     curs_set(0);
 
     dir = STOP;
+    gameOver = 0;
+    frameDelayUs = INITIAL_DELAY_US;
     x = WIDTH / 2;
     y = HEIGHT / 2;
-
-    srand(time(0));
-    fruitX = rand() % WIDTH;
-    fruitY = rand() % HEIGHT;
-
+    nTail = 0;
     score = 0;
+
+    srand((unsigned int)time(NULL));
+    PlaceFruit();
 }
 
 void Draw() {
@@ -52,6 +88,7 @@ void Draw() {
                     if (tailX[k] == j && tailY[k] == i) {
                         printw("o");
                         print = 1;
+                        break;
                     }
                 }
                 if (!print)
@@ -68,6 +105,7 @@ void Draw() {
         printw("#");
     printw("\n");
     printw("Score: %d\n", score);
+    printw("Flechas para mover, 'x' para salir.\n");
 
     refresh();
 }
@@ -77,38 +115,45 @@ void Input() {
     if (ch != ERR) {
         switch (ch) {
         case KEY_LEFT:
-            dir = LEFT;
+            if (dir != RIGHT)
+                dir = LEFT;
             break;
         case KEY_RIGHT:
-            dir = RIGHT;
+            if (dir != LEFT)
+                dir = RIGHT;
             break;
         case KEY_UP:
-            dir = UP;
+            if (dir != DOWN)
+                dir = UP;
             break;
         case KEY_DOWN:
-            dir = DOWN;
+            if (dir != UP)
+                dir = DOWN;
             break;
         case 'x':
-            endwin();
-            exit(0);
+        case 'X':
+            gameOver = 1;
             break;
         }
     }
 }
 
 void Logic() {
-    int prevX = tailX[0];
-    int prevY = tailY[0];
-    int prev2X, prev2Y;
-    tailX[0] = x;
-    tailY[0] = y;
-    for (int i = 1; i < nTail; i++) {
-        prev2X = tailX[i];
-        prev2Y = tailY[i];
-        tailX[i] = prevX;
-        tailY[i] = prevY;
-        prevX = prev2X;
-        prevY = prev2Y;
+    if (nTail > 0) {
+        int prevX = tailX[0];
+        int prevY = tailY[0];
+        int prev2X, prev2Y;
+
+        tailX[0] = x;
+        tailY[0] = y;
+        for (int i = 1; i < nTail; i++) {
+            prev2X = tailX[i];
+            prev2Y = tailY[i];
+            tailX[i] = prevX;
+            tailY[i] = prevY;
+            prevX = prev2X;
+            prevY = prev2Y;
+        }
     }
     switch (dir) {
     case LEFT:
@@ -132,29 +177,53 @@ void Logic() {
 
     for (int i = 0; i < nTail; i++)
         if (tailX[i] == x && tailY[i] == y) {
-            endwin();
-            exit(0);
+            gameOver = 1;
+            return;
         }
 
     if (x == fruitX && y == fruitY) {
         score += 10;
-        fruitX = rand() % WIDTH;
-        fruitY = rand() % HEIGHT;
+        if (nTail < MAX_TAIL) {
+            nTail++;
+        }
 
-        nTail++;
-        usleep((unsigned int)((1 - (nTail / 5.0)) * 100000));
+        if (frameDelayUs > MIN_DELAY_US) {
+            frameDelayUs -= DELAY_STEP_US;
+            if (frameDelayUs < MIN_DELAY_US) {
+                frameDelayUs = MIN_DELAY_US;
+            }
+        }
+
+        if (nTail + 1 >= WIDTH * HEIGHT) {
+            gameOver = 1;
+            return;
+        }
+
+        PlaceFruit();
     }
 }
 
 int main() {
     Setup();
-    while (1) {
+    while (!gameOver) {
         Draw();
-        Input();
-        Logic();
-        usleep(100000);
+    Input();
+    Logic();
+    SleepMicroseconds(frameDelayUs);
     }
 
+    nodelay(stdscr, FALSE);
+    mvprintw(HEIGHT + 4, 0, "Fin del juego. Puntaje final: %d", score);
+    mvprintw(HEIGHT + 5, 0, "Presiona cualquier tecla para salir...");
+    refresh();
+    getch();
     endwin();
     return 0;
+}
+
+void SleepMicroseconds(int microseconds) {
+    struct timespec ts;
+    ts.tv_sec = microseconds / 1000000;
+    ts.tv_nsec = (microseconds % 1000000) * 1000;
+    nanosleep(&ts, NULL);
 }
